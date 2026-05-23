187b65d1a2f51c20b60bab9d3140caa5db211a0b
Mascot <165539450+Mascot-11@users.noreply.github.com>
2026-05-23 14:26:09 +0545

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,10 +3,22 @@ export default async (req, res) => {
     const { default: workerHandler } = await import('../dist/server/index.js');
 
     const url = new URL(req.url || '/', `http://${req.headers.host}`);
+    
+    // Read body for POST requests
+    let body = undefined;
+    if (!['GET', 'HEAD'].includes(req.method)) {
+      body = await new Promise((resolve, reject) => {
+        const chunks = [];
+        req.on('data', chunk => chunks.push(chunk));
+        req.on('end', () => resolve(Buffer.concat(chunks)));
+        req.on('error', reject);
+      });
+    }
+
     const fetchRequest = new Request(url.toString(), {
       method: req.method,
       headers: req.headers,
-      body: ['GET', 'HEAD'].includes(req.method) ? undefined : req,
+      body: body?.length ? body : undefined,
     });
 
     const response = await workerHandler.fetch(fetchRequest, {}, {});
